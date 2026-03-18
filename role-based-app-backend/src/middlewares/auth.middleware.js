3d6a0d38d8014caad61ded3c7f4353804a83b8fc
gesim-dave123 <christiandavegesim@gmail.com>
2026-03-19 02:43:22 +0800

    Feat: Contains all the auth middlwares

---
+++ role-based-app-backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+require("dotenv/config");
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Access token required" });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ error: "Invalid or expired token" });
+    req.user = user;
+    next();
+  });
+};
+
+const authorizeRole = (role) => {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res
+        .status(403)
+        .json({ error: "Access denied: insufficient permission" });
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateToken, authorizeRole };
