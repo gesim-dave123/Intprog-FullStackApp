8fc30616d5c19df2f17d1503781a7a00e5cf86a0
gesim-dave123 <christiandavegesim@gmail.com>
2026-03-19 02:42:10 +0800

    Feat: Created a mockup dtaabase file for departments

---
+++ role-based-app-backend/src/databaseMockUp/department.Storage.js
@@ -0,0 +1,19 @@
+let departments = [
+  {
+    dept_id: 1,
+    dept_name: "Engineering",
+    description: "Handles system developemnt",
+  },
+  {
+    dept_id: 2,
+    dept_name: "HR",
+    description: "Handles employee relations",
+  },
+  {
+    dept_id: 3,
+    dept_name: "IT",
+    description: "Build and maintains the IT infrustructure",
+  },
+];
+
+module.exports = departments;
