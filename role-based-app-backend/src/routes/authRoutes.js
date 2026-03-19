5ee36325d4373f7937c98121041a616aaeb122ac
gesim-dave123 <christiandavegesim@gmail.com>
2026-03-20 01:41:06 +0800

    Feat:Added the verify email routes

--- role-based-app-backend/src/routes/authRoutes.js
+++ role-based-app-backend/src/routes/authRoutes.js
@@ -3,12 +3,13 @@ const {
   registerUser,
   login,
   logout,
+  verifyEmail,
 } = require("../controllers/auth.controller.js");
-// const { authenticateToken } = require("../middlewares/auth.middleware.js");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/verify-email", verifyEmail);
 
 module.exports = router;
